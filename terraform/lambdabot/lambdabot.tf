d9394220378d979b374c527276c2a17bb699b5a2
Eric Conlon <eric@takt.com>
2017-08-26 11:34:41 -0700

    proxy integration

--- terraform/lambdabot/lambdabot.tf
+++ terraform/lambdabot/lambdabot.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 data "terraform_remote_state" "s3" {
   backend = "s3"
   config {
@@ -25,4 +27,25 @@ resource "aws_lambda_function" "lambdabot_lambda" {
   role             = "${data.terraform_remote_state.roles.iam_for_lambda_arn}"
   handler          = "lambda.handler"
   runtime          = "nodejs6.10"
+}
+
+resource "aws_api_gateway_rest_api" "lambdabot_api" {
+  name = "lambdabot"
+}
+
+resource "aws_api_gateway_method" "lambdabot_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.lambdabot_api.id}"
+  resource_id   = "${aws_api_gateway_rest_api.lambdabot_api.root_resource_id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambdabot_integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.lambdabot_api.id}"
+  resource_id             = "${aws_api_gateway_rest_api.lambdabot_api.root_resource_id}"
+  http_method             = "${aws_api_gateway_method.lambdabot_method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambdabot_lambda.arn}/invocations"
+  credentials             = "${data.terraform_remote_state.roles.iam_for_gateway_arn}"
 }
--- terraform/roles/roles.tf
+++ terraform/roles/roles.tf
@@ -18,6 +18,35 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+resource "aws_iam_role" "iam_for_gateway" {
+  name = "iam_for_gateway"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "gateway_attachment" {
+  role       = "${aws_iam_role.iam_for_gateway.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaFullAccess"
+}
+
 output "iam_for_lambda_arn" {
   value = "${aws_iam_role.iam_for_lambda.arn}"
+}
+
+output "iam_for_gateway_arn" {
+  value = "${aws_iam_role.iam_for_gateway.arn}"
 }
